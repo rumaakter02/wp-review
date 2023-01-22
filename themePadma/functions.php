27e10ed59f126a46d9bb347d7ee1353f45cca3bb
rumaakter02 <116252191+rumaakter02@users.noreply.github.com>
2023-01-22 14:37:58 +0600

    Add files via upload

---
+++ themePadma/functions.php
@@ -0,0 +1,18 @@
+<?php
+wp_enqueue_style( 'style-name', get_stylesheet_uri() );
+wp_enqueue_style( 'style-boot', get_template_directory_uri() . '/assests/css/bootstrap.min.css' );
+wp_enqueue_script( 'script-name', get_template_directory_uri() . '/assests/js/bootstrap.bundle.min.js', array(), '1.0.0', true );
+
+
+
+add_theme_support( 'title-tag' );
+add_theme_support( 'custom-logo' );
+add_theme_support( 'post-thumbnails' );
+
+register_sidebar([
+    'name'=> 'Bd Logo',
+    'id'=> 'bdlogo',
+    'before_widget'=>'',
+    'after_widget'=>'',
+])
+?>
